88cd5c73a06a39eec813d0242be8c4ab6984d91a
Allen Chan <zhanbz@qq.com>
2015-04-14 12:44:10 +0800

    Update bubsort.c
    Modify the algorithm error in buble sort.

--- bubsort.c
+++ bubsort.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
 void bubsort(int array[], int n)
 {
-	int i, j, tmp, swap = 0;
-	for (i = 0; i < n - 1; i++) {
-		for (j = i + 1; j < n; j++) {
-			if (array[i] > array[j]) {
-				tmp = array[i];
-				array[i] = array[j];
-				array[j] = tmp;		
-				swap = 1;
+	int i, j, tmp, swap_flag = 0;
+	for (i = 0; i < n - 1; ++i) {
+		for (j = n - 1; j > i; --j) {
+			if (array[j] < array[j - 1]) {
+				swap(&array[j], &array[j - 1]);	
+				swap_flag = 1;
 			}		
 		}
 		
-		if (!swap) break;
+		if (!swap_flag) break;
 	}
 
 }
